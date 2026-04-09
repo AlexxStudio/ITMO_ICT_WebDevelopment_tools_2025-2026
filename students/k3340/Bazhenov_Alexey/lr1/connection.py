d43eb50a53bfecf854d708f9018f104a37bdaf5f
Alexey Bazhenov <alexey931931@gmail.com>
2026-04-09 10:26:16 +0300

    Практика 1.3: вынесена конфигурация в .env

--- students/k3340/Bazhenov_Alexey/lr1/connection.py
+++ students/k3340/Bazhenov_Alexey/lr1/connection.py
@@ -1,7 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from sqlmodel import SQLModel, Session, create_engine
 
 
-DATABASE_URL = "postgresql://postgres:Spec10spec!@localhost/finance_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
